23c7e20dbca4a6b88e88f31c7d247d987151dfa5
nugongshou110 <444857416@qq.com>
2015-11-24 20:06:13 +0800

    优化Handler
    优化Handler防止内存泄露

--- app/src/main/java/com/zhangqi/jingdongrefreshlistview/PullToRefreshActivity.java
+++ app/src/main/java/com/zhangqi/jingdongrefreshlistview/PullToRefreshActivity.java
@@ -3,6 +3,7 @@ package com.zhangqi.jingdongrefreshlistview;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -10,25 +11,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PullToRefreshActivity extends Activity implements JingDongListView.OnJingDongRefreshListener {
-	private JingDongListView mListView;
+	private static JingDongListView mListView;
 	private List<String> mDatas;
-	private ArrayAdapter<String> mAdapter;
+	private static ArrayAdapter<String> mAdapter;
 	private final static int REFRESH_COMPLETE = 0;
 
-	private Handler mHandler = new Handler(){
-		public void handleMessage(android.os.Message msg) {
-			switch (msg.what) {
-			case REFRESH_COMPLETE:
-				mListView.setOnRefreshComplete();
-				mAdapter.notifyDataSetChanged();
-				mListView.setSelection(0);
-				break;
+	private InterHandler mInterHandler = new InterHandler();
 
-			default:
-				break;
+	private static class InterHandler extends Handler{
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+				case REFRESH_COMPLETE:
+					mListView.setOnRefreshComplete();
+					mAdapter.notifyDataSetChanged();
+					mListView.setSelection(0);
+					break;
+
+				default:
+					break;
 			}
-		};
-	};
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +57,7 @@ public class PullToRefreshActivity extends Activity implements JingDongListView.
 				try {
 					Thread.sleep(3000);
 					mDatas.add(0, "new data");
-					mHandler.sendEmptyMessage(REFRESH_COMPLETE);
+					mInterHandler.sendEmptyMessage(REFRESH_COMPLETE);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
